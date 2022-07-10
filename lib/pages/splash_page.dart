c8c32f9318e4bcf7ea781021c5225b7d7af36c1f
Amit Chaudhary <itheamc@gmail.com>
2022-07-10 18:29:54 +0545

    Seventh Commit - Added Icon

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
+import 'dart:math';
 
-import 'package:financial_terms/components/a_loading_indicator.dart';
+import 'package:financial_terms/config/a_theme.dart';
 import 'package:financial_terms/handler/session/session_handler.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../config/routes.dart';
 
@@ -19,17 +21,67 @@ class _SplashPageState extends State<SplashPage> {
   void initState() {
     super.initState();
 
-    Timer(const Duration(milliseconds: 2000), _handleNextRoute);
+    Timer(Duration(milliseconds: Random().nextInt(2500) + 1000),
+        _handleNextRoute);
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final theme = Theme.of(context);
+
+    return Scaffold(
       extendBody: true,
       extendBodyBehindAppBar: true,
-      body: ALoadingIndicator(
-        label: "Financial Terms",
-        size: 75.0,
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/icon/ftlogo.png",
+                  width: Get.width * 0.3,
+                  height: Get.width * 0.3,
+                ),
+                ShaderMask(
+                  blendMode: BlendMode.srcATop,
+                  shaderCallback: (Rect bounds) {
+                    return LinearGradient(
+                      colors: [
+                        theme.primaryColor,
+                        theme.colorScheme.secondary,
+                      ],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                    ).createShader(bounds);
+                  },
+                  child: Text(
+                    "Financial Terms",
+                    style: GoogleFonts.jost(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(
+                bottom: FinancialTermsAppTheme.paddingSmall,
+              ),
+              child: Text(
+                "Code with ❤ by @mit",
+                style: GoogleFonts.jost(
+                  fontSize: 10.0,
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
